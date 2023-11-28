ddcf9f06655c1ff3e258c76961b9391c6f990411
herohasan0 <kahraman.h@outlook.com>
2023-11-29 00:15:43 +0300

    create a file about interfaces

---
+++ newbie/interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main( ) {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot){
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// Very custom logic for generating greeting
+	return "Hi There"
+}
+
+func (spanishBot) getGreeting() string {
+	// Very custom logic for generating greeting
+	return "Hola"
+}
